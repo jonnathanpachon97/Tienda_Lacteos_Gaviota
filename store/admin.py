b4c8d2ecd76d9d99a121f741896fea92fb3e74b9
Jonnathan Pachon <jonnathanpachon10@gmail.com>
2024-12-11 18:02:44 -0500

    Configuración del admin y plantillas para mostrar imágenes de productos. Se añadió la visualización de miniaturas en el panel de administración y en el catálogo.

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Producto
 
 # Register your models here.
+
+class ProductoAdmin(admin.ModelAdmin):
+    model = Producto
+    list_display = ['nombre', 'descripcion', 'precio', 'categoria', 'stock']
+    search_fields = ['nombre']
+
+admin.site.register(Producto, ProductoAdmin)
